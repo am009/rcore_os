e832c484ab8e212fe5a1aaa63335fd24366ea6e2
am009 <634960348@qq.com>
2020-07-07 09:11:57 +0800

    Day3

---
+++ tmp.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // Statements here are executed when the compiled binary is called
+    (let b = a = 1);;
+    ;
+    // Print text to the console
+    println!("Hello World!{:?}", a);
+}

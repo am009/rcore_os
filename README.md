04fb670168542edfd8572f86dbb0f52957956342
am009 <634960348@qq.com>
2020-07-12 12:07:59 +0800

    day 9

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # rcore_os
 Learning rust and operating system...
+
+learning-cargo/ 是Rust编程之道的第10章跟着做
+
+lists/ 是跟着做用rust写双链表的教程
+https://rust-unofficial.github.io/too-many-lists/
+
+hardways/ 是笨办法学C的一些练习, 外面的几个rs文件也是
+
+rustlings/
--- test.rs
+++ test.rs
@@ -1,3 +1,17 @@
+fn choose<'a, 'b>(is_a: bool, a: &'a str, b: &'b str) -> Enum<'b, 'a> {
+    if is_a {
+        Enum::A(a)
+    } else {
+        Enum::B(b)
+    }
+}
+
+#[derive(Debug)]
+enum Enum<'a, 'b> {
+    A(&'a str),
+    B(&'b str)
+}
+
 fn main() {
     let mut s = std::string::String::from("123123");
     let s2 = &mut s;
